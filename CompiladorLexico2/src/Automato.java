5edb81502deaee31318733917595f5f8e546ae5c
Michael <mb-nascimento@hotmail.com>
2017-09-07 20:25:21 -0300

    Classe automato criada

---
+++ CompiladorLexico2/src/Automato.java
@@ -0,0 +1,131 @@
+
+public class Automato {
+    
+    
+    
+    public void inicio(String texto, int i){
+       
+        if(texto.charAt(i) == '#'){
+            
+            
+            //código
+            
+        }
+        
+        else if(Character.isLetter(texto.charAt(i))){
+            
+            //código
+            
+        }
+        else if(Character.isDigit(texto.charAt(i)) ){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == '$'){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == '"'){
+            
+            //código
+            
+        }
+        //Precisa ver se isso funciona
+        else if(String.valueOf(texto.charAt(i)).equals("'")){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == '='){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == '+'){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == '*'){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == '/'){
+            
+            //códgo
+            
+        }
+        
+        else if(texto.charAt(i) == '>'){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == '<'){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == '{'){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == '}'){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == '('){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == ','){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == ':'){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == ';'){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == '-'){
+            
+            //código
+            
+        }
+        
+        else if(texto.charAt(i) == '!'){
+            
+            //código
+            
+        }
+        
+        
+    }
+    
+}
